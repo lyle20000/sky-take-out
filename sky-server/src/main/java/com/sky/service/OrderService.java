acbc103cb492cb9175e7cb296ba418383d1a9943
lyle2000 <1345869472@qq.com>
2024-09-27 23:28:26 +0800

    再来一单功能代码

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -50,4 +50,11 @@ public interface OrderService {
      * @param id
      */
     void userCancelById(Long id) throws Exception;
+
+    /**
+     * 再来一单
+     *
+     * @param id
+     */
+    void repetition(Long id);
 }
